3d8cf65a1c2ee783afabe4b5abbe963798eec4b5
Michael Corey Munn <coreymunn3@gmail.com>
2021-09-10 15:16:10 -0400

    Added product type with queries, and dummy data

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
-const { mainCards } = require('./db');
+const { mainCards, products } = require('./db');
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -10,14 +10,32 @@ const typeDefs = gql`
     image: String
   }
 
+  type Product {
+    id: ID!
+    slug: String!
+    image: String!
+    name: String!
+    rating: Float
+    price: Float
+    description: [String!]!
+    stock: Int!
+    onSale: Boolean
+  }
+
   type Query {
     mainCards: [MainCard]
+    products: [Product]
+    product(slug: String!): Product
   }
 `;
 
 const resolvers = {
   Query: {
     mainCards: () => mainCards,
+    products: () => products,
+    product: (parent, args, context) => {
+      return products.find((p) => p.slug === args.slug);
+    },
   },
 };
 
